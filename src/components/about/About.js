5bc8a1b6429e5745ee2c111054385eea797853fb
Tyler Eftink <42481212+T-BoneMadSkrilla@users.noreply.github.com>
2019-01-20 14:22:02 -0600

    Changed some of the wording and spell check

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -8,9 +8,9 @@ class About extends Component{
         return(
             <div className="aboutFlex">
                 <div className="indexZero">
-                    My story starts like most people, I didn't know what I wanted to do when I was 18 going to college. I ended up studying GIS and graduated in 2015 from  Sam Houston State University. I throughly enjoyed taking large sets of data and making sense of it with maps. 
+                    I graduated in 2015 from Sam Houston State University with a degree in geography and a certificate in GIS. I thoroughly enjoyed taking large sets of data and making sense of it with maps. 
                     <br></br>
-                    Their weren't many jobs in GIS when I graduated but I was lucky enough to land my first real job in business development at a small-ish software company. It wasn't before I realized my creative/technical bug was unfullied and that's when I started to look into coding.   
+                    Their weren't many jobs in GIS when I graduated but I was lucky enough to land my first real job in business development at a small software company. I loved having a difficult job but it wasn't long before I realized my creative/technical bug was unfullied and that's when I started to dabble with coding.   
                 </div>
                 <br></br>
                
@@ -20,13 +20,13 @@ class About extends Component{
                
                <br></br>
                 <div className="secondParagraph">
-                    In early 2018, I started an eCommerce company with Shopify. Unfortunately, sales dried up and the business went up in flames. I learned a lot though; I learned that I really enjoyed building sites and I was curious about the code behind all the cool things I could do with Shopify and WordPress. That's when I started learning what has turned into my love language, JavaScript.   
+                    In early 2018, I started an eCommerce company with Shopify. Unfortunately, sales dried up and my marketing budget was nearly zero. It was an incredible learning experience; I learned that I really enjoyed building sites and I was curious about the code behind all the cool things I could do with Shopify and WordPress. That's when I started learning what has turned into my love language, JavaScript.   
                 </div>
                    
                 
                 <br></br>
                 <div className="secondParagraph">
-                   It took me a few months but I decided to go to a coding bootcamp to sharpen my skills. After a lot of early mornings and late nights, I graduated to DevMountain in December 2018. 
+                   It took me a few months but I decided to go to a coding bootcamp to sharpen my skills. After a lot of early mornings and late nights, I graduated from DevMountain in December 2018. 
                 
                    
                 </div>
