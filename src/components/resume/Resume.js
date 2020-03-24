e6fec270826bfa7760f2f6e9ccfeb820bff78680
unknown <tylereftink@gmail.com>
2020-03-24 15:23:38 -0500

    updated resume

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -5,7 +5,7 @@ class Resume extends Component{
     render(){
         return(
             <div className="centerResume">
-                <img className="containResume" src={require('../../pictures/updatedresumepic3.PNG')}></img>
+                <img className="containResume" src={require('../../pictures/resume2020.PNG')}></img>
             </div>
         )
     }
